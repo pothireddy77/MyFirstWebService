8d35587866a4e1ea5c134d389003b8a5101a25e8
Ravi Kumar Reddy Pothireddy 6115 <RAVIKUMARRP@omnicell.com>
2023-05-11 16:57:37 -0700

    Pavan changes

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -32,7 +32,7 @@ namespace WebApplication1.Controllers
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)],
-                Description = "New Service Test"
+                Description = "PAvan Vikash Ravi"
             })
             .ToArray();
         }
